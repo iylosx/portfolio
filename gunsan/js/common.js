3fca907b9f0b218f4229c5c06bb2857b87271b92
iylosx <plzsirrla@gmail.com>
2025-12-24 15:40:22 +0900

    251224 군산

--- gunsan/js/common.js
+++ gunsan/js/common.js
@@ -39,5 +39,44 @@ $(document).ready(function(){
 
     })
 
+    $('header .header_main .gnb_open').on('click', function(){
+        $('header').addClass('open_mo')
+    })
+    $('header .header_main .gnb_wrap .gnb_close').on('click', function(){
+        $('header').removeClass('open_mo')
+    })
+
+    $('header .header_main .gnb_wrap ul.depth1 > li').on('click', function(e){
+        if(device_status == 'mo'){
+        e.preventDefault();
+            if($('header .header_main .gnb_wrap ul.depth1 > li').hasClass('select')){
+                $('header .header_main .gnb_wrap ul.depth1 > li').removeClass('select')
+                $(this).addClass('select')
+            }
+        }
+    })
+    $('header .header_main .gnb_wrap .depth1 > li > .depth2_wrap > ul.depth2 > li > a').on('click', function(){
+        if(device_status == 'mo'){
+
+            gnb_open = $(this).parent().hasClass('open')
+
+            if(gnb_open == true){ //열려있다면
+                $(this).parent().removeClass('open')
+                $(this).next().slideUp()
+            }else{
+                $('header .header_main .gnb_wrap .depth1 > li > .depth2_wrap > ul.depth2 > li').removeClass('open')
+                $('header .header_main .gnb_wrap .depth1 > li > .depth2_wrap > ul.depth2 > li > ul.depth3').slideUp()
+                $(this).parent().addClass('open')
+                $(this).next().slideDown()
+            }
+
+
+        }
+
+
+    })
+        
+            
+
 
 })//js end
